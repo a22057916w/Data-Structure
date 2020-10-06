5ab3b34c904747595a313935e8d82a41babcfb7a
naitea9527 <willy@willylaptop.localdomain>
2020-10-06 17:31:12 +0800

    correct func name and syntax error

--- recursion/fibonacci/fibonacci.cpp
+++ recursion/fibonacci/fibonacci.cpp
@@ -1,19 +1,19 @@
-#include <iostram>
+#include <iostream>
 using namespace std;
 
 typedef unsigned long long LLU;
 
-LLU fabonacci(int n) {
+LLU fibonacci(int n) {
   if(n == 0)
     return 0;
   if(n == 1)
     return 1;
-  return fabonacci(n - 1) + fabonacci(n - 2);
+  return fibonacci(n - 1) + fibonacci(n - 2);
 }
 
 int main() {
   int n;
 
   while(cin >> n && n >= 0)
-    cout << fabonacci(n) << endl;
+    cout << fibonacci(n) << endl;
 }
