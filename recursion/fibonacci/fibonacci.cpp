ba86367b13a9fa2a4903017ef7b8f67572e863ec
naitea9527 <willy@willylaptop.localdomain>
2020-10-06 20:01:31 +0800

    adding non-recursive solution

--- recursion/fibonacci/fibonacci.cpp
+++ recursion/fibonacci/fibonacci.cpp
@@ -3,6 +3,9 @@ using namespace std;
 
 typedef unsigned long long LLU;
 
+#define SOL nREC
+#if(SOL == REC)
+
 LLU fibonacci(int n) {
   if(n == 0)
     return 0;
@@ -16,4 +19,23 @@ int main() {
 
   while(cin >> n && n >= 0)
     cout << fibonacci(n) << endl;
+  return 0;
 }
+
+#else
+#define MAXN 1005
+
+int main() {
+
+  LLU fib[MAXN] = {0};
+  fib[0] = 0, fib[1] = 1;
+
+  for(int i = 2; i < MAXN; i++)
+    fib[i] = fib[i - 1] + fib[i - 2];
+
+  int n;
+  while(cin >> n && n >= 0)
+    cout << fib[n] << endl;
+}
+
+#endif
