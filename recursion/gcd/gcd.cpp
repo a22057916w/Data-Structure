dd795ccaa235c3684adaaab2a3ee246e4bdbb442
naitea9527 <willy@willylaptop.localdomain>
2020-10-07 17:39:43 +0800

    adding non-recursive sol

--- recursion/gcd/gcd.cpp
+++ recursion/gcd/gcd.cpp
@@ -3,6 +3,9 @@ using namespace std;
 
 typedef unsigned long long LLU;
 
+#define REC 0
+
+#if(REC == 1)
 // It's presumably a >= b but it's okay for the other way around.
 LLU gcd(int a, int b) {
   if(b == 0)
@@ -10,6 +13,17 @@ LLU gcd(int a, int b) {
   else
     return gcd(b, a % b);
 }
+#else
+// Using loop to implement gcd
+LLU gcd(int a, int b) {
+    while(b > 0) {
+      int tmp = b;
+      b = a % b;
+      a = tmp;
+    }
+    return a;
+}
+#endif
 
 int main() {
   int a, b;
