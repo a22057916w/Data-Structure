406252f50a7dff6015b2b58a02803615c4507b9f
naitea9527 <willy@willylaptop.localdomain>
2020-10-07 21:23:39 +0800

    mk recursive sol for permutation

---
+++ recursion/permutation/perm.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+using namespace std;
+
+typedef unsigned long long LLU;
+
+const string alphabet = "abcdefghijklmnopqrstuvwxyz";
+
+void perm(string s, int pos, int n, int len) {
+  if(pos == n)
+    cout << s.substr(0, len) << endl;
+  else {
+    for(int i = pos; i < n; i++) {
+        swap(s[pos], s[i]);
+        perm(s, pos + 1, n, len);
+        swap(s[pos], s[i]);
+    }
+  }
+  return;
+}
+
+int main() {
+  int n, len;
+  while(cin >> n && (0 < n && n <= 26)) {
+    string s = alphabet;
+    len = n;    // record the length of substring s[0] + ... + s[n]
+    perm(s, 0, n, len);  // the function would print all the combinations
+  }
+  return 0;
+}
