e4fc297e09195bc1e2dc0f748ec49f3ab836e1c3
naitea9527 <willy@willylaptop.localdomain>
2020-10-07 18:23:20 +0800

    developing non-recursive solutoin

--- recursion/ackermann/ack.cpp
+++ recursion/ackermann/ack.cpp
@@ -3,6 +3,10 @@ using namespace std;
 
 typedef unsigned long long LLU;
 
+#define REC 0
+
+#if(REC == 1)
+// Produced according to the Ackermann's function directly
 LLU ack(int m, int n) {
   if(m == 0)
     return n + 1;
@@ -11,6 +15,17 @@ LLU ack(int m, int n) {
   else
     return ack(m - 1, ack(m, n - 1));
 }
+#else
+// Since the recursive method may cause stackoverflow, we need another solution
+LLU ack(int m, int n) {
+    LLU ans = 0;
+    for(int i = m; i >= 0; i--) {
+      for(int j = n; j >= 0; j--) {
+        for(int k = m; k >= 0; k--)
+      }
+    }
+}
+#endif
 
 int main() {
   int m, n;
