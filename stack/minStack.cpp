76affae5f4d1cf303d697f7001c728a072b07b26
a22057916w <a22057916w@gmail.com>
2020-12-09 19:46:46 +0800

    dev minStack

---
+++ stack/minStack.cpp
@@ -0,0 +1,92 @@
+#include <iostream>
+#include <stack>
+using namespace std;
+
+class MinStack {
+private:
+    stack<int> dataStack;
+    stack<int> minStack;
+  public:
+    MinStack(){};
+
+    void Push(int x);
+    void Pop();
+    bool isEmpty();
+    int Top();
+    int getSize();
+};
+
+void MinStack::Push(int x) {
+  dataStack.push(x);
+  if(minStack.empty() || x < minStack.top())
+    minStack.push(x);
+  else
+    minStack.push(minStack.top());
+}
+
+void MinStack::Pop() {
+  if(isEmpty())
+    cout << "The stack is empty" << endl;
+  else {
+    dataStack.pop();
+    minStack.pop();
+  }
+}
+
+bool MinStack::isEmpty() {
+  return dataStack.empty();
+}
+
+int MinStack::Top() {
+  if(isEmpty()) {
+    cout << "The stack is empty" << endl;
+    return -1;
+  }
+  else
+    return dataStack.top();
+}
+
+int MinStack::getSize() {
+  if(isEmpty()) {
+    cout << "The stack is empty" << endl;
+    return -1;
+  }
+  else
+    return dataStack.size();
+}
+
+int main() {
+  MinStack s;
+  s.Pop();
+  s.Push(6);
+  cout << "\ntop: " << s.Top() << "\nmin: " << s.getMin() << endl;
+
+  s.Push(13);
+  cout << "\ntop: " << s.Top() << "\nmin: " << s.getMin() << endl;
+
+  s.Push(4);
+  cout << "\ntop: " << s.Top() << "\nmin: " << s.getMin() << endl;
+
+  s.Push(9);
+  cout << "\ntop: " << s.Top() << "\nmin: " << s.getMin() << sendl;
+
+  s.Push(1);
+  cout << "\ntop: " << s.Top() << "\nmin: " << s.getMin() << endl;
+
+  s.Pop();
+  cout << "\ntop: " << s.Top() << "\nmin: " << s.getMin() << endl;
+
+  s.Pop();
+  cout << "\ntop: " << s.Top() << "\nmin: " << s.getMin() << endl;
+
+  s.Pop();
+  cout << "\ntop: " << s.Top() << "\nmin: " << s.getMin() << endl;
+
+  s.Pop();
+  cout << "\ntop: " << s.Top() << "\nmin: " << s.getMin() << endl;
+
+  s.Pop();
+  cout << "\ntop: " << s.Top() << "\nmin: " << s.getMin() << endl;
+
+  return 0;
+}
