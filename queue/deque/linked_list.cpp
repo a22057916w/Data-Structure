ecb1b5d1bf94ccc88ce49bbf251e26731c5bd851
a22057916w <a22057916w@gmail.com>
2020-12-14 17:25:08 +0800

    implement member functions

--- queue/deque/linked_list.cpp
+++ queue/deque/linked_list.cpp
@@ -29,6 +29,96 @@ public:
     int getSize();
 };
 
-void MyDeque:push_front(int x) {
+void MyDeque::push_front(int x) {
+  if(isEmpty()) {
+    front = new QueueNode(x);
+    back = front;
+    size++;
+    return;
+  }
 
+  QueueNode *newFront = new QueueNode(x);
+  newFront->next = front;
+  front->prev = newFront;
+  front = newFront;
+  size++;
+}
+
+void MyDeque::push_back(int x) {
+  if(isEmpty()) {
+    front = new QueueNode(x);
+    back = front;
+    size++;
+    return;
+  }
+
+  QueueNode *newBack = new QueueNode(x);
+  newBack->prev = back;
+  back->next = newBack;
+  back = newBack;
+  size++;
+}
+
+void NyDeque::pop_back() {
+  if(isEmpty())
+    cout << "The queue is empty." << endl;
+  else {
+    QueueNode *delNode = back;
+    back = back->prev;
+
+    if(back == NULL)
+      front = back = NULL;
+    else
+      back->next = NULL;
+
+    delete delNode;
+    delNode = NULL;
+
+    size--;
+  }
+}
+
+void MyDeque::pop_front() {
+  if(isEmpty())
+    cout << "The queue is empty." << endl;
+  else {
+    QueueNode *delNode = front;
+    front = front->next;
+
+    if(front == NULL)
+      front = back = NULL;
+    else
+      front->prev = NULL;
+
+    delete delNode;
+    delNode = NULL;
+
+    size--;
+  }
+}
+
+bool MyDeque::isEmpty() {
+  return size == 0;
+}
+
+int MyDeque::getFront() {
+  if(isEmpty()) {
+    cout << "The queue is empty." << endl;
+    return -1;
+  }
+
+  return front->data;
+}
+
+int MyDeque::getBack() {
+  if(isEmpty()) {
+    cout << "The queue is empty." << endl;
+    return -1;
+  }
+
+  return back->data;
+}
+
+int MyDeque::getSize() {
+  return size;
 }
