a4ce9e46ff4bbc868ec50623b0af52d665976a8c
a22057916w <a22057916w@gmail.com>
2021-01-07 22:59:10 +0800

    deving Construstor

--- tree/binary_tree/impelement/array.cpp
+++ tree/binary_tree/impelement/array.cpp
@@ -1,5 +1,24 @@
 #include <iostream>
+#include <sstream>
 using namespace std;
+
+class BinaryTree {
+private:
+  int capacity;
+  char *tree;
+
+  void doubleCapacity();
+public:
+  BinaryTree(): capacity(10) {
+      tree = new char[capacity];
+  }
+  BinaryTree(const char *str) {
+    streamstring ss;
+    ss << str;
+  }
+
+  void insert(char data);
+};
 
 int main() {
   return 0;
