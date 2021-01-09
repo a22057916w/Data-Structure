4dbda3e7a7dedff1d45a2cef3b7916c43b63253a
a22057916w <a22057916w@gmail.com>
2021-01-09 15:52:14 +0800

    fix syntax error

--- tree/binary_tree/impelement/array.cpp
+++ tree/binary_tree/impelement/array.cpp
@@ -180,12 +180,12 @@ void BinaryTree::swap() {
   }
 }
 
-bool equal(BinaryTree &src) {
-  if(size != src->size || node != src->node)
+bool BinaryTree::equal(BinaryTree &src) {
+  if(size != src.size || node != src.node)
     return false;
 
-  for(int i = 0; i < size;; i++)
-    if(tree[i] != src->tree[i])
+  for(int i = 0; i < size; i++)
+    if(tree[i] != src.tree[i])
       return false;
   return true;
 }
