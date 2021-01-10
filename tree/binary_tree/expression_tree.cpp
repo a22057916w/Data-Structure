5c54b6e6c9c33416e09a0ff841ccb5e9394d5728
a22057916w <a22057916w@gmail.com>
2021-01-10 10:57:56 +0800

    declare ExprssionTree class and deving TreeNode

--- tree/binary_tree/expression_tree.cpp
+++ tree/binary_tree/expression_tree.cpp
@@ -1,6 +1,16 @@
 #include <iostream>
 using namespace std;
 
+class ExpressionTree;
+class TreeNode {
+private:
+    char data;
+    TreeNode *parent;
+    TreeNode *left, *right;
+public:
+  TreeNode(): parent(0), left(0), right(0), data('\0') {};
+}
+
 int main() {
   return 0;
 }
