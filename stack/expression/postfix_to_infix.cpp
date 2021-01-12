d0e9eb55d86c4adcaace2a1a256233b3ab9303f5
a22057916w <a22057916w@gmail.com>
2021-01-12 22:00:43 +0800

    imp and finish postfix_to_infix.cpp

--- stack/expression/postfix_to_infix.cpp
+++ stack/expression/postfix_to_infix.cpp
@@ -1,13 +1,34 @@
 #include <iostream>
+#include <cctype>
 #include <string>
 #include <stack>
 using namespace std;
 
 string PostfixToInfix(const string postfix) {
-  
+  stack<string> infix;
+
+  for(int i = 0; i < postfix.length(); i++) {
+
+    if(isalpha(postfix[i])) {
+      string op(1, postfix[i]);
+      infix.push(op);
+    }
+    else {
+      string op1 = infix.top();
+      infix.pop();
+      string op2 = infix.top();
+      infix.pop();
+
+      infix.push("(" + op2 + postfix[i] + op1 + ")");
+    }
+  }
+
+  return infix.top();
 }
 
 int main() {
+  string postfix = "xyae+/%";
 
+  cout << PostfixToInfix(postfix) << endl;
   return 0;
 }
