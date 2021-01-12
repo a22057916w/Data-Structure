41dc39a06cb65ad421a1d52412ff86ae1ddd875f
a22057916w <a22057916w@gmail.com>
2021-01-12 15:27:12 +0800

    dev driver code

--- stack/expression/driver.cpp
+++ stack/expression/driver.cpp
@@ -1,6 +1,8 @@
-#include <isotream>
-
 #include <infix_to_postfix.h>
+#include <infix_to_prefix.h>
+
+#include <iostream>
+#include <string>
 
 using namespace std;
 
@@ -8,7 +10,10 @@ int main() {
   string exp = "(a-b/c)*(a/k-l)";
 
   string postExp = InfixToPostfix(exp);
+  string preExp = InfixToPrefix(exp);
+
   cout << postExp << endl;
+  cout << preExp << endl;
 
   return 0;
 }
