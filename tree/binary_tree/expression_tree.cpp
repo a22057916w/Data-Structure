90082b293db66da3ce9cf7bddbff81d397a0c78f
a22057916w <a22057916w@gmail.com>
2021-01-13 17:20:57 +0800

    declaration of class ExpressionTree, TreeNode, and there members

--- tree/binary_tree/expression_tree.cpp
+++ tree/binary_tree/expression_tree.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <cctype>
+#include <stack>
 using namespace std;
 
 class ExpressionTree;
@@ -9,7 +11,27 @@ private:
     TreeNode *left, *right;
 public:
   TreeNode(): parent(0), left(0), right(0), data('\0') {};
+  TreeNode(char data): parent(0), left(0), right(0), data(data) {};
+  
+  friend class ExpressionTree;
 }
+
+class ExpressionTree {
+private:
+  TreeNode *root;
+
+  void inorder(TreeNode *curr);
+  void preorder(TreeNode *curr);
+  void postorder(TreeNode *curr);
+
+public:
+  ExpressionTree(const string postfix);
+  ExpressionTree(const string prefix);
+
+  void inorder();
+  void preorder();
+  void postorder();
+};
 
 int main() {
   return 0;
