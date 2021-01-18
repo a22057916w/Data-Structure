7ce373bc7283ceaf26282aa7184d2672182ebebe
a22057916w <a22057916w@gmail.com>
2021-01-18 13:45:25 +0800

    add members to both class

--- tree/binary_tree/BST.cpp
+++ tree/binary_tree/BST.cpp
@@ -14,12 +14,21 @@ public:
   TreeNode(): parent(0), left(0), right(0), key(0), element("") {};
   TreeNode(int k, string e): parent(0), left(0), right(0), key(k), element(e) {};
 
+  int getKey() {return key;}
+  string getElement() {return element;}
+
   friend class BST;
 };
 
 class BST {
 private:
     TreeNode *root;
+
 public:
-    BST()
+    BST(): root(0) {};
+
+    TreeNode *search(int key);
+    void insert(int key);
+
+    void inorder();
 };
