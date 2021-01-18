6c360690e29096cfe499a73be36e2eaa9f22e456
a22057916w <a22057916w@gmail.com>
2021-01-18 15:39:03 +0800

    imp BST::insert()

--- tree/binary_tree/BST.cpp
+++ tree/binary_tree/BST.cpp
@@ -25,11 +25,15 @@ private:
     TreeNode *root;
 
     TreeNode *search(int key, TreeNode *curr);
+
+    TreeNode *insert(TreeNode *curr, TreeNode *newNode)
+
 public:
     BST(): root(0) {};
 
     TreeNode *search(int key);
-    void insert(int key);
+
+    void insert(int key, element);
 
     void inorder();
 };
@@ -41,7 +45,7 @@ TreeNode *BST::search(int key) {
 
 TreeNode *BST::search(int key, TreeNode *curr) {
 
-  // Base Cases: root(curr) is null or key is present at root(curr) 
+  // Base Cases: root(curr) is null or key is present at root(curr)
   if(curr == NULL || curr->key = key)
     return curr;
 
@@ -52,4 +56,25 @@ TreeNode *BST::search(int key, TreeNode *curr) {
   // Key is smaller than root's key
   else
     return search(key, curr->right);
+}
+
+void BST::insert(int key, string element) {
+  insert(root, new TreeNode(key, element));
+}
+
+TreeNode* BST::insert(TreeNode *curr, TreeNode *newNode) {
+
+  // If the root is empty or reach the bottom of a leaf
+  if(curr == NULL)
+    return newNode;
+
+  // Key is smaller than root's key
+  if(newNode->key <= curr->key)
+    root->left  = insert(root->left, newNode);
+
+  // Key is greater than root's key
+  else
+    root->right = insert(root->right, newNode);
+
+  return root;
 }
