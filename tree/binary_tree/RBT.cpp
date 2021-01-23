dc6fdfaaf5a51ac3490e47fe790efcf789a0d8e4
a22057916w <a22057916w@gmail.com>
2021-01-23 23:08:59 +0800

    init class RBT, TreeNode

--- tree/binary_tree/RBT.cpp
+++ tree/binary_tree/RBT.cpp
@@ -2,7 +2,24 @@
 #include <string>
 using namespace std;
 
+class RBT;
+class TreeNode {
+private:
+    TreeNode *left, *right;
+    TreeNode *parent;
 
+    int key, color;
+
+public:
+
+
+    friend class RBT;
+};
+
+class RBT {
+private:
+  TreeNode *root;
+};
 
 int main() {
   return 0;
