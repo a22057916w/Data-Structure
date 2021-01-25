06ffac1c886ca2101b6426e409100a8cfea63b3a
a22057916w <a22057916w@gmail.com>
2021-01-25 13:37:14 +0800

    write concept of RBT

--- tree/binary_tree/RBT.cpp
+++ tree/binary_tree/RBT.cpp
@@ -1,3 +1,13 @@
+/*
+Rules That Every Red-Black Tree Follows:
+1. Every node has a colour either red or black.
+2. The root of tree is always black.
+3. There are no two adjacent red nodes (A red node cannot have a red parent or
+   red child).
+4. Every path from a node (including root) to any of its descendant NULL node
+   has the same number of black nodes.
+*/
+
 #include <iostream>
 #include <string>
 using namespace std;
