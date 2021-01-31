12a40cbc678832a0db9742a603a1e87df9f970a4
a22057916w <a22057916w@gmail.com>
2021-01-31 22:50:47 +0800

    new testcase

--- tree/binary_tree/RBT.cpp
+++ tree/binary_tree/RBT.cpp
@@ -246,5 +246,21 @@ void AVL_TREE::levelorder() {
 }
 
 int main() {
+  RBTree tree;
+
+  tree.insert(7);
+  tree.insert(6);
+  tree.insert(5);
+  tree.insert(4);
+  tree.insert(3);
+  tree.insert(2);
+  tree.insert(1);
+
+  cout << "Inoder Traversal of Created Tree\n";
+  tree.inorder();
+
+  cout << "\n\nLevel Order Traversal of Created Tree\n";
+  tree.levelOrder();
+
   return 0;
 }
