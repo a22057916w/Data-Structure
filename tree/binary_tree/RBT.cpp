b7a71f18edd6a970ae0e499a2afe9c3e4e478473
a22057916w <a22057916w@gmail.com>
2021-01-31 18:40:44 +0800

    deving insert()

--- tree/binary_tree/RBT.cpp
+++ tree/binary_tree/RBT.cpp
@@ -37,9 +37,10 @@ private:
   TreeNode *root;
 
   // modifying functions
+  TreeNode *insert(TreeNode *root, int key);
   TreeNode *leftRotation(TreeNode *curr);
   TreeNode *rightRotation(TreeNode *curr);
-  
+
   // traversal function
   void inorder(TreeNode *curr);
 
@@ -47,12 +48,41 @@ public:
   RBT(): root(0) {}
 
   // modifying functions
-  void insert(const int &key);
+  void insert(int key);
 
   // traversal function
   void inorder();
   void levelorder();
 };
+
+// **************** implementation of modifying functions ********************
+/*
+Algorithm (Insertion)
+Let x be the newly inserted node.
+
+1. Perform standard BST insertion and make the colour of newly inserted nodes as RED.
+2. If x is the root, change the colour of x as BLACK (Black height of complete
+   tree increases by 1).
+3. Do the following if the color of x’s parent is not BLACK and x is not the root.
+   a) If x’s uncle is RED (Grandparent must have been black from property 4)
+     (i) Change the colour of parent and uncle as BLACK.
+     (ii) Colour of a grandparent as RED.
+     (iii) Change x = x’s grandparent, repeat steps 2 and 3 for new x.
+    b) If x’s uncle is BLACK, then there can be four configurations for x,
+       x’s parent (p) and x’s grandparent (g) (This is similar to AVL Tree)
+      (i) Left Left Case (p is left child of g and x is left child of p)
+      (ii) Left Right Case (p is left child of g and x is the right child of p)
+      (iii) Right Right Case (Mirror of case i)
+      (iv) Right Left Case (Mirror of case ii)
+*/
+void RBT::insert(int key) {
+  insert(root, key);
+}
+
+TreeNode* RBT::insert(TreeNode *root, int key) {
+
+  if()
+}
 
 int main() {
   return 0;
