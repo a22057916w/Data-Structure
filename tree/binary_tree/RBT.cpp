be0acefdc74425cc34d50b659e6a41421bdc8649
a22057916w <a22057916w@gmail.com>
2021-02-23 19:35:33 +0800

    erase old testcase

--- tree/binary_tree/RBT.cpp
+++ tree/binary_tree/RBT.cpp
@@ -281,7 +281,7 @@ void RBT::deleteNode(int key) {
   else if(color == BLACK)
     FixDeletion(x);
 
-  
+
   if(x->key == INT_MIN) {
     if(x == x->parent->right)
       x->parent->right = NULL;
@@ -465,21 +465,6 @@ void RBT::levelorder() {
 }
 
 int main() {
-  /*RBT tree;
-
-  tree.insert(7);
-  tree.insert(6);
-  tree.insert(5);
-  tree.insert(4);
-  tree.insert(3);
-  tree.insert(2);
-  tree.insert(1);
-
-  cout << "Inoder Traversal of Created Tree\n";
-  tree.inorder();
-
-  cout << "\n\nLevel Order Traversal of Created Tree\n";
-  tree.levelorder();*/
 
   RBT tree;
 
