67b0c068d47d0b28600cdc57c0f7b10aaadcd701
a22057916w <a22057916w@gmail.com>
2021-03-07 15:42:36 +0800

    new HashChain Class and private members

--- hash_table/stl.cpp
+++ hash_table/stl.cpp
@@ -15,6 +15,19 @@ public:
   friend class HashChain
 };
 
+class HashChain {
+private:
+    int size;     // size of table
+    int count;    // number of data
+
+    // Hash table with linked list. vector for array, list for linked list chaining
+    vector<list<Dict> > table;
+
+    int PreHashing(string key_str);     // turn string_type_key to int_type_key
+    int HashFunction(string key_str);   // using Division method  
+public:
+};
+
 
 int main() {
   return 0;
