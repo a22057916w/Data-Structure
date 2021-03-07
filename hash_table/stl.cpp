7d62ea72816c2290a4652aa6fe728f1b8195d429
a22057916w <a22057916w@gmail.com>
2021-03-07 15:32:21 +0800

    new a Dictionary class for hash table

--- hash_table/stl.cpp
+++ hash_table/stl.cpp
@@ -4,6 +4,16 @@
 #include <string>
 using namespace std;
 
+class Dict {
+private:
+    String key;
+    String value;
+public:
+  Dict(): key(""), value("") {};
+  Dict(string key, string value): key(key), value(value) {};
+
+  friend class HashChain 
+}
 int main() {
   return 0;
 }
