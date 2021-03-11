412f25a58cbabb68f0902914b5393ff22015a01a
a22057916w <a22057916w@gmail.com>
2021-03-11 19:13:01 +0800

    imp clscc Dict

--- hash_table/quadratic_probing.cpp
+++ hash_table/quadratic_probing.cpp
@@ -2,6 +2,16 @@
 #include <string>
 using namespace std;
 
+class Dict {
+  string key;
+  string value;
+
+  Dict(): key(""), value("") {};
+  Dict(string key, string val) key(key), value(val) {};
+
+  friend class HashChain;
+}
+
 int main() {
   return 0;
 }
