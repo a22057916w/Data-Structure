0a79212b46757f932a46f2762dea2d7c3922538d
a22057916w <a22057916w@gmail.com>
2021-03-12 14:20:24 +0800

    new testcase

--- hash_table/quadratic_probing.cpp
+++ hash_table/quadratic_probing.cpp
@@ -95,5 +95,28 @@ void display() {
 }
 
 int main() {
+
+  HashOpenAddress hash(8);          // probing sequence:
+  hash.insert(33, "blue");          // 1,2,4,7,3,0,6,5 -> 1
+  hash.insert(10, "yellow");        // 2,3,5,0,4,1,7,6 -> 2
+  hash.insert(77, "red");           // 5,6,0,3,7,4,2,1 -> 5
+  hash.insert(2, "white");          // 2,3,5,0,4,1,7,6 -> 3
+  hash.display();
+  hash.insert(8, "black");          // 0,1,3,6,2,7,5,4 -> 0
+  hash.insert(47, "gray");          // 7,0,2,5,1,6,4,3 -> 7
+  hash.insert(90, "purple");        // 2,3,5,0,4,1,7,6 -> 4
+  hash.insert(1, "deep purple");    // 4,5,7,2,6,3,1,0 -> 6
+  hash.display();
+  hash.insert(15, "green");         // hash table overflow
+
+  cout << "number#90 is " << hash.search(90) << "\n\n";
+
+  hash.deleteKey(90);
+  cout << "after deleting (90,purple):\n";
+  cout << "number#90 is " << hash.search(90) << "\n";
+
+  hash.insert(12, "orange");        // 4,5,7,2,6,3,1,0 -> 4
+  hash.display();
+
   return 0;
 }
