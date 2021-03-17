e11041e6ff2a71ce7313bd1079e5dba6f17c6fe5
a22057916w <a22057916w@gmail.com>
2021-03-18 01:16:57 +0800

    deving class MinHeap

--- tree/binary_tree/heap.cpp
+++ tree/binary_tree/heap.cpp
@@ -13,6 +13,14 @@ public:
     int parent(int index);
     int left(int index);
     int right(int index);
+
+    int extractMin();
+    void decreaseKey(int index, int new_val);
+
+    int getMin();
+
+    void insert(int key);
+    void deletKey(int index);
 }
 
 int main() {
