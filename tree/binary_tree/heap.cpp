690f0c6318dc72ed5e3f0671e68d0e9a8d4246dc
a22057916w <a22057916w@gmail.com>
2021-03-18 00:59:53 +0800

    deving class MInHeap

---
+++ tree/binary_tree/heap.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+class MinHeap {
+private:
+    vector<int> heap;
+public:
+    MinHeap() {};
+    MinHeap(int size) {
+      heap.resize(size);
+    }
+
+    int parent(int index);
+    int left(int index);
+    int right(int index);
+}
