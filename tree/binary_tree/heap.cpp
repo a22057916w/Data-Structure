62923c6398574a31bda9a2c738074eb4fb264060
a22057916w <a22057916w@gmail.com>
2021-03-22 16:17:33 +0800

    new testcase

--- tree/binary_tree/heap.cpp
+++ tree/binary_tree/heap.cpp
@@ -106,5 +106,20 @@ void MinHeap::MinHeapify(int i) {
 }
 
 int main() {
+  MinHeap h(11);
+
+  h.insertKey(3);
+  h.insertKey(2);
+  h.deleteKey(1); 
+  h.insertKey(15);
+  h.insertKey(5);
+  h.insertKey(4);
+  h.insertKey(45);
+
+  h.extractMin();
+  cout << h.getMin() << " ";
+  h.decreaseKey(2, 1);
+  cout << h.getMin();
+
   return 0;
 }
