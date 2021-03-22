1a24add87ed8a861dad2724d656b9acf2c32bd43
a22057916w <a22057916w@gmail.com>
2021-03-22 14:37:47 +0800

    imp parent(), right(), and left()

--- tree/binary_tree/heap.cpp
+++ tree/binary_tree/heap.cpp
@@ -24,6 +24,18 @@ public:
     void deletKey(int index);
 };
 
+int MinHeap::parent(int index) {
+  return (index - 1) / 2;
+}
+
+int MinHeap::right(int index) {
+  return (index * 2) + 2;
+}
+
+int MinHeap::left(int index) {
+  return (index * 2) + 1;
+}
+
 int main() {
   return 0;
 }
