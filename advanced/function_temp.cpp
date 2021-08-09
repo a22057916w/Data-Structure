4a1d79d974b017e95d4b3402af4679355c5267e9
a22057916w <a22057916w@gmail.com>
2021-08-09 21:26:39 +0800

    update comment

--- advanced/function_temp.cpp
+++ advanced/function_temp.cpp
@@ -17,7 +17,7 @@ T myMin(T x, T y) {
 int main(int argc, char* argv[])
 {
     cout << myMax<int>(3, 7) << endl; // Call myMax for int
-    cout << myMin<int>(3, 7) << endl; // Call myMax for int
+    cout << myMin<int>(3, 7) << endl; // Call myMin for int
     cout << myMax<double>(3.0, 7.0) << endl; // call myMax for double
     cout << myMax<char>('g', 'e') << endl; // call myMax for char
 
