c8582de2d79609204a5257a1414e59884647f296
a22057916w <a22057916w@gmail.com>
2021-08-10 20:49:34 +0800

    new all sector examples except sys

---
+++ advanced/memory/all.cpp
@@ -0,0 +1,17 @@
+#include <cstdio>
+const int global_x = 1;  // 儲存於 data 區段(唯讀區域)
+int global_y = 2;        // 儲存於 data 區段(可讀寫區域)
+int global_z;            // 儲存於 bss 區段
+int main() {
+  const static int x = 1; // 儲存於 data 區段（唯讀區域）
+  static int y = 1;       // 儲存於 data 區段（可讀寫區域）
+  static int z;           // 儲存於 bss 區段
+  int w = 1;              // 儲存於 stack 區段
+
+  // 儲存於 heap 區段
+  char *buf = (char*) malloc(sizeof(char) * 100);
+  // ...
+  free(buf);
+
+  return 0;
+}
