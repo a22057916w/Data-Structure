8992a241d6abc0518954f063ac1ab0bf79b9f34b
a22057916w <a22057916w@gmail.com>
2021-10-15 13:30:55 +0800

    imp bubblesort

---
+++ sort/bubblesort.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using std::vector;
+using std::cout;
+using std::endl;
+
+void bubblesort(vector<int> &v) {
+  for(int i = 0; i < v.size(); i++)
+    for(int j = 0; j < v.size() - 1; j++)
+      if(v[j] > v[j + 1])
+        std::swap(v[j], v[j+1]);
+}
+
+int main(int argc, char **argv) {
+  vector<int> nums(10, 0);
+
+  for(auto &e : nums)
+    e = rand() % 10;
+
+  for(auto e : nums)
+    cout << e << " ";
+  cout << endl;
+
+  bubblesort(nums);
+
+  for(auto e : nums)
+    cout << e << " ";
+  return 0;
+}
