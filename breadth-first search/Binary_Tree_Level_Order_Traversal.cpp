38a140cfdf01c2ac7228a3ec4fbc1df138985028
naitea9527 <a22057916w@gmail.com>
2022-05-25 15:35:47 +0800

    BFS x Queue x BinaryTree

---
+++ breadth-first search/Binary_Tree_Level_Order_Traversal.cpp
@@ -0,0 +1,47 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+#include <iostream>
+#include <queue>
+
+class Solution {
+public:
+    vector<vector<int>> levelOrder(TreeNode* root) {
+      vector<vector<int>> result;
+
+      if(root == NULL)
+        return result;
+
+      queue<TreeNode*> q;
+      q.push(root);
+
+      while(!q.empty()) {
+        int n = q.size();
+        vector<int> level;
+
+        for(int i = 0; i < n; i++) {
+          TreeNode *node = q.front();
+          q.pop();
+
+          level.push_back(node->val);
+
+          if(node->left)
+            q.push(node->left);
+          if(node->right)
+            q.push(node->right);
+        }
+
+        result.push_back(level);
+      }
+
+      return result;
+    }
+};
