72c070b3e9bb720a4e625d989cef4f75e85fb275
naitea9527 <a22057916w@gmail.com>
2022-06-01 17:19:57 +0800

    BFS x Binary Search x Matrix x Optimal Find

--- BFS/Escape_the_Spreading_Fire.cpp
+++ BFS/Escape_the_Spreading_Fire.cpp
@@ -1,3 +1,12 @@
+// https://leetcode.com/problems/escape-the-spreading-fire/
+
+/*
+The idea is:
+  step 1: spread the fire with BFS
+  step 2: check edge cases
+  step 3: find the optimal time using Binary Search
+*/
+
 class Solution {
 public:
     int r[4] = {1, 0, 0, -1}, c[4] = {0, 1, -1, 0};
@@ -5,8 +14,8 @@ public:
     // return fireTime table by BFS
     vector<vector<int>> fireSpreadTime(vector<vector<int>> grid) {
 
-      // declare the frieTime table as the same size as grid
-      vector<vector<int>> fireTime(grid.size(), vector<int>(grid[0].size(), 0));
+      // set the value to infinitly large
+      vector<vector<int>> fireTime(grid.size(), vector<int>(grid[0].size(), INT_MAX / 2));
 
       // set fireTime table by BFS queue
       queue<pair<int, int>> fire;
@@ -16,8 +25,8 @@ public:
           for(int j = 0; j < grid[0].size(); j++)
               if(grid[i][j] == 1) {
                   fire.push({i, j});
-                  fireTime[i][j] = 0;
-                }
+                  fireTime[i][j] = 0;   // set starting fire position to zero timing
+              }
 
       // set fireTime table
       while(!fire.empty()) {
@@ -27,13 +36,13 @@ public:
 
         int newTime = fireTime[fx][fy] + 1;
         for(int i = 0; i < 4; i++) {
-          fx = fx + r[i];
-          fy = fy + c[i];
+          int x = fx + r[i];
+          int y = fy + c[i];
 
-            if((fx >= 0 && fx < grid.size()) && (fy >= 0 && fy < grid[0].size()) && (grid[fx][fy] == 0)) {
-                grid[fx][fy] = 1;
-                fireTime[fx][fy] = newTime;
-                fire.push({fx, fy});
+            if((x >= 0 && x < grid.size()) && (y >= 0 && y < grid[0].size()) && grid[x][y] == 0) {
+                grid[x][y] = 1;
+                fireTime[x][y] = newTime;
+                fire.push({x, y});
             }
         }
       }
@@ -45,19 +54,16 @@ public:
       // get fireTime table/grid
       vector<vector<int>> fireTime = fireSpreadTime(grid);
 
-      for(int i = 0; i < grid.size(); i++) {
-        for(int j = 0; j < grid[0].size(); j++)
-          cout << fireTime[i][j] << " ";
-        cout << endl;
-      }
-
+      // check if the fire can't reach safehouse and player
       int maxPossibleTime = grid.size() * grid[0].size();
       if(canEscape(maxPossibleTime, fireTime, grid))
         return 1E9;
 
+      // check if it is impossible to reach safehouse
       if(!canEscape(0, fireTime, grid))
         return -1;
 
+      // check the rest possibe time by binary-search
       int maxWaitTime = 0;
       int L = 0, R = maxPossibleTime;
       while(L < R) {
@@ -69,10 +75,14 @@ public:
         else
           R = M - 1;
       }
+      // check edge condition
+      if(canEscape(L, fireTime, grid))
+        maxWaitTime = L;
 
       return maxWaitTime;
     }
 
+    // determine if the player can escape to safehouse
     bool canEscape(int waitTime, vector<vector<int>>& fireTime, vector<vector<int>> grid) {
 
       // declare the playerTime table as the same size as grid
@@ -81,6 +91,7 @@ public:
       queue<pair<int, int>> q;
       q.push({0, 0});
 
+      // do BFS
       while(!q.empty()) {
         pair<int, int> pp = q.front();
         q.pop();
@@ -88,16 +99,17 @@ public:
 
         int newTime = playerTime[px][py] + 1;
         for(int i = 0; i < 4; i++) {
-          px = px + r[i];
-          py = py + c[i];
+          int x = px + r[i];
+          int y = py + c[i];
 
-          if((px == grid.size() - 1 && py = grid[0].size() - 1) && newTime <= fireTime[px][py])
+          // special case: the fire and player can reach the safehouse at the saem time
+          if((x == grid.size() - 1 && y == grid[0].size() - 1) && newTime <= fireTime[x][y])
             return true;
 
-          if((px >= 0 && px < grid.size()) && (py >= 0 && py < grid[0].size()) && (grid[px][py] == 0) && newTime < fireTime[px][py]) {
-              grid[px][py] = 1;
-              playerTime[px][py] = newTime;
-              q.push({px, py});
+          if((x >= 0 && x < grid.size()) && (y >= 0 && y < grid[0].size()) && (grid[x][y] == 0) && newTime < fireTime[x][y]) {
+              grid[x][y] = 1;
+              playerTime[x][y] = newTime;
+              q.push({x, y});
           }
         }
       }
