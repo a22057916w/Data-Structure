f0b84cacead747da92cd241d7c3ca2d0d7036f15
naitea9527 <a22057916w@gmail.com>
2022-06-02 15:11:41 +0800

    MST x Union Find x Kruskal's Algorithm

--- MST/Min_Cost_to_Connect_All_Points.cpp
+++ MST/Min_Cost_to_Connect_All_Points.cpp
@@ -1,34 +1,67 @@
 #include <alogrithm>
 #include <utility>
 
+class Edge {
+public:
+  int a, b;
+  int dist;
+
+  Edge(int a, int b, int dist): a(a), b(b), dist(dist) {};
+
+  bool operator< (const Edge &e) {
+    return this->dist < e.dist;
+  }
+};
+
 class Solution {
 public:
+  vector<int> p;    // disjoint-set, track each point's parent
+
   int minCostConnectPoints(vector<vector<int>>& points) {
     int n = points.size();
 
     vector<Edge> edge;
 
+    // calculating edge and sort
     for(int i = 0; i < n; i++)
-      for(int j = 0; j < n; j++) {
-        int dist = abs(p[i][0] - p[j][0]) + abs(p[i][1] - p[j][1])
-        edge.push_back(new Edge({p[i][0], p[i][1]}, {p[j][0], p[j][1]}, dist));
+      for(int j = i + 1; j < n; j++) {
+        int dist = abs(points[i][0] - points[j][0]) + abs(points[i][1] - points[j][1]);
+        edge.push_back(Edge(i, j, dist));
+
       }
-    sort(edge);
+    sort(edge.begin(), edge.end());
 
-    for(auto& e: edge)
-      cout << e << " ";
-    return 0;
+    int cost = kruskal(edge);
+    return cost;
   }
-};
 
-class Edge {
-public:
-  pair<int, int> a, b;
-  int dist;
+  int kruskal(vector<Edge>& edge) {
+    int n = edge.size();
 
-  Edge(): a(a), b(b), dist(dist) {};
+    p.resize(n + 1);
+    for(int i = 0; i < n + 1; i++)
+      p[i] = i;
 
-  bool operator<(Edge &e1, Edge &e2) {
-    retrun e1.dist < e2.dist;
+    int cost = 0;
+    for(int i = 0; i < n; i++) {
+      int a = edge[i].a;
+      int b = edge[i].b;
+
+      // if the vertex has the same root(form a circle), ignore it
+      if(find(a) == find(b))
+        continue;
+
+      // mrege two tree by assign them to the same root
+      p[find(a)] = find(b);
+
+      cost += edge[i].dist;
+    }
+
+    return cost;
+  }
+
+  int find(int x) {
+    // p[x] = find(p[x]): assign new parent while searching to imporve efficiency
+    return x == p[x] ? x : p[x] = find(p[x]);
   }
 };
