ed876f1630fde6febeb4e3b0621080db038b8541
naitea9527 <a22057916w@gmail.com>
2022-06-07 16:18:07 +0800

    Update ACM_Contest_and_Blackout.cpp

---
+++ MST/ACM_Contest_and_Blackout.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+using namespace std;
+
+
+int main() {
+  int testcase;
+
+  cin >> testcase;
+  while(testcase--) {
+
+  }
+
+  return 0;
+}
