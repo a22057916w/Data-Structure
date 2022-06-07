f1aa7604980b761450d6c88ebf484b13be4c8ce4
naitea9527 <a22057916w@gmail.com>
2022-06-07 18:50:06 +0800

    imp prim's algo

--- MST/ACM_Contest_and_Blackout.cpp
+++ MST/ACM_Contest_and_Blackout.cpp
@@ -1,15 +1,65 @@
 #include <iostream>
 #include <vector>
 #include <queue>
+#include <utility>
+#include <algorithm>
 using namespace std;
 
+int prim(int v, vector<vector<int>> &edges) {
+
+  // for pair comparison: c++ compares the first elements and only if they are equivalent, compares the second elements.
+  priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+  vector<pair<int, int>> adj[v];
+  vector<bool> vis(v, false);
+
+  // initiate the adjacency list
+  for(int i = 0; i < edges.size(); i++) {
+    int u = edges[i][0] - 1;
+    int v = edges[i][1] - 1;
+    int dist = edges[i][2];
+
+    adj[u].push_back({v, dist});
+    adj[v].push_back({u, dist});
+  }
+
+  int vertex = edges[0][0];   // set a strating point
+  pq.push({0, 0});    // given a initial trivial value (0-dist, node-random)
+
+  int cost = 0;
+  while(!pq.empty()) {
+    auto p = pq.top();  // the top of pq is the most lishtwage edge
+    pq.pop();
+    int weight = p.first;
+    int vertex = p.second;
+
+    if(!vis[vertex]) {
+      cost += weight;
+      vis[vertex] = true;
+
+      // find the adjcent node for vertex then add the {dist, node} to pq
+      for(auto e: adj[vertex])
+        if(!vis[e.first])
+          pq.push({e.second, e.first});
+    }
+  }
+  return cost;
+}
 
 int main() {
   int testcase;
 
   cin >> testcase;
   while(testcase--) {
+    int v, e;
+    cin >> v >> e;
 
+    vector<vector<int>> edges(e, vector<int>(3));
+    for(int i = 0; i < e; i++)
+      for(int j = 0; j < 3; j++)
+        cin >> edges[i][j];
+
+    int MSTcost = prim(v, edges);
+    cout << MSTcost << endl;
   }
 
   return 0;
