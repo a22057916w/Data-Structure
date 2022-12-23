ac6277ab0456b987437f7ae17a30eb0e2d4a5c08
a22057916w <a22057916w@gmail.com>
2022-12-23 18:52:49 +0800

    first finished version

---
+++ flow/ford_fulkerson.cpp
@@ -0,0 +1,95 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <queue>
+using namespace std;
+
+typedef vector<vector<int>> vii;
+typedef vector<int> vi;
+
+class GraphFlowNetWork {
+private:
+  int V;      // number of vertex
+  vii adj;   // adjacent matrix
+public:
+  GraphFlowNetWork(int n): V(n) {
+    adj.resize(n);
+    for(int i = 0; i < n; i++)
+      adj[i].resize(n, 0);
+  }
+
+  void addEdge(int u, int v, int capacity);
+  int FordFulkerson(int src, int tml);
+  bool BFSfindExistingPath(vii graphResidual, vi &predecessor, int s, int t);
+
+};
+
+
+void GraphFlowNetWork::addEdge(int u, int v, int capacity) {
+  adj[u][v] = capacity;
+}
+
+int GraphFlowNetWork::FordFulkerson(int src, int tml) {
+  vii graphResidual(adj);
+  vi predecessor(V, -1);
+  int maxFlow = 0;
+
+  while(BFSfindExistingPath(graphResidual, predecessor, src, tml)) {
+
+    int mincapacity = 1E9;
+    for(int v = tml; v != src; v = predecessor[v]) {
+      int u = predecessor[v];
+      mincapacity = min(graphResidual[u][v], mincapacity);
+    }
+
+    maxFlow += mincapacity;
+
+    for(int v = tml; v != src; v = predecessor[v]) {
+      int u = predecessor[v];
+      graphResidual[u][v] -= mincapacity;
+      graphResidual[v][u] += mincapacity;
+    }
+  }
+
+  return maxFlow;
+}
+
+bool GraphFlowNetWork::BFSfindExistingPath(vii graphResidual, vi &predecessor, int s, int t) {
+  vi visited(V, 0);
+  predecessor.resize(V, -1);
+
+  queue<int> q;
+
+  q.push(s);
+  visited[s] = 1;
+  while(!q.empty()) {
+    int u = q.front();
+    q.pop();
+
+    for(int v = 0; v < V; v++)
+      if(graphResidual[u][v] != 0 && visited[v] != 1) {
+        q.push(v);
+        visited[v] = 1;
+        predecessor[v] = u;
+
+        if(v == t)
+          return true;
+      }
+  }
+
+  return false;
+}
+
+int main() {
+  GraphFlowNetWork g(6);
+
+  g.addEdge(0, 1, 9);g.addEdge(0, 3, 9);
+  g.addEdge(1, 2, 3);g.addEdge(1, 3, 8);
+  g.addEdge(2, 4, 2);g.addEdge(2, 5, 9);
+  g.addEdge(3, 2, 7);g.addEdge(3, 4, 7);
+  g.addEdge(4, 2, 4);g.addEdge(4, 5, 8);
+
+  cout << g.FordFulkerson(0, 5) << endl;    // 指定source為vertex(0), termination為vertex(5)
+
+  return 0;
+}
