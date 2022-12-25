25d79e72eec43dcfe2809ce4f2d166cf955d2776
a22057916w <a22057916w@gmail.com>
2022-12-25 12:41:50 +0800

    deving vertex_cover algo

--- vertexCover/vertex_cover.cpp
+++ vertexCover/vertex_cover.cpp
@@ -1,14 +1,36 @@
 #include <iostream>
 #include <algorithm>
-#include <vector>
+#include <list>
 using namespace std;
 
 class Graph {
 private:
-  int V;
-  vector<int> adj;
+  int V;              // number of vertex
+  list<int> *adj;     // edges is much less than V*V
+public:
+  Graph(int n): V(n) {
+    adj = new list<int>[V];
+  }
+
+  void addEdge(int u, int v);
+  void getVertexCover()
 };
 
+void Graph::addEdge(int u, int v) {
+  // undirected graph
+  adj[u].push_back(v);
+  adj[v].push_back(u);
+}
+
+void Graph::getVertexCover() {
+  bool visited[V] = {false};
+
+  for(int u = 0; u < v; u++) {
+
+  }
+}
+
 int main() {
+
   return 0;
 }
