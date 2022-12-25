a8346b608301308bc79d7f4833033ebe592d9f5f
naitea9527 <a22057916w@gmail.com>
2022-12-25 14:51:42 +0800

    vertex cover using 2-approach algorithm

--- vertexCover/vertex_cover.cpp
+++ vertexCover/vertex_cover.cpp
@@ -14,7 +14,7 @@ public:
   }
 
   void addEdge(int u, int v);
-  void getVertexCover()
+  int getVertexCover();
 };
 
 void Graph::addEdge(int u, int v) {
@@ -23,16 +23,25 @@ void Graph::addEdge(int u, int v) {
   adj[v].push_back(u);
 }
 
-void Graph::getVertexCover() {
+int Graph::getVertexCover() {
   bool visited[V] = {false};
-  int apr_min = 0;
+  int apr_min = 0;      // record the covered vertex
 
-  for(int u = 0; u < v; u++) {
+  // Consider all edges one by one
+  for(int u = 0; u < V; u++) {
 
+    // An edge is only picked when both visited[u] and visited[v]
+    // are false
     if(visited[u] == false) {
 
-      for(int it = adj[u].begin(), it != adj[u].end(); it++) {
+      // Go through all adjacents of u and pick the first not
+      // yet visited vertex (We are basically picking an edge
+      // (u, v) from remaining edges.
+      for(auto it = adj[u].begin(); it != adj[u].end(); it++) {
         int v = *it;
+
+        // Add the vertices (u, v) to the result set
+        // all edges from/to them would be ignored
         if(visited[v] == false) {
           visited[v] = true;
           visited[u] = true;
@@ -53,6 +62,15 @@ void Graph::getVertexCover() {
 }
 
 int main() {
+  // Create a graph given in the above diagram
+  Graph g(7);
+  g.addEdge(0, 1);
+  g.addEdge(0, 2);
+  g.addEdge(1, 3);
+  g.addEdge(3, 4);
+  g.addEdge(4, 5);
+  g.addEdge(5, 6);
 
+  cout << g.getVertexCover() << endl;
   return 0;
 }
