d96065d5b46df43b40c64b7d21fed1f1f84f1569
naitea9527 <a22057916w@gmail.com>
2025-11-11 15:26:41 +0800

    +bubblesort in C

---
+++ sort/bubblesort.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+void bubblesort(int* arr, int arrSize) {
+    for(int i = 0; i < arrSize - 1; i++) {
+        bool swapped = false;
+        for(int j = 0; j < arrSize - 1 - i; j++) {
+            if(arr[j] > arr[j + 1]) {
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+                swapped = true;
+            }
+        }
+        if(!swapped)
+            return;
+    }
+}
+
+void print_array(int* arr, int arrSize) {
+    printf("Array: ");
+    for(int i = 0; i < arrSize; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+}
+
+int main() {
+    int arr[] = {64, 34, 25, 12, 22, 11, 90};
+    int arrSize = sizeof(arr) / sizeof(arr[0]);
+
+    printf("Before sorting:\n");
+    print_array(arr, arrSize);
+
+    bubblesort(arr, arrSize);
+
+    printf("After sorting:\n");
+    print_array(arr, arrSize);
+
+    return 0;
+}
